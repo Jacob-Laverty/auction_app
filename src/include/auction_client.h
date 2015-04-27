b3156f55547be0ce6349f2664532a2714d902b92
Jacob-Laverty <jacob.laverty@gmail.com>
2015-04-27 02:06:57 -0400

    Project setup

---
+++ src/include/auction_client.h
@@ -0,0 +1,6 @@
+#ifndef AUCTION_CLIENT_H
+#define AUCTION_CLIENT_H
+#include "auction_app.h"
+
+
+#endif
---
+++ src/include/auction_server.h
@@ -0,0 +1,10 @@
+#ifndef AUCTION_SERVER_H
+#define AUCTION_SERVER_H
+#include 
+
+
+#define PORT 7227
+#define SERVER_DEFAULT_HOST "127.0.0.1"
+#define SERVER_FAMILY AF_INET 
+#define SERVER_HOST INADDR_ANY
+#endif
